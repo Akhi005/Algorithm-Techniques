3f3b06a64e155f56ad0799fb2b02d39f3781314f
Akhi005 <77240828+Akhi005@users.noreply.github.com>
2021-11-05 20:20:08 +0600

    Update knapsack 01 (Dp Top down Approach) .cpp

--- knapsack 01 (Dp Top down Approach) .cpp
+++ knapsack 01 (Dp Top down Approach) .cpp
@@ -1,31 +1,23 @@
-#include <bits/stdc++.h> // O(n*W)
+#include <bits/stdc++.h> 
 using namespace std;
 #define ll long long
-ll weight[10000], price[10000]  ,K[1000][1000];
-ll knapsack(ll W, ll weight[], ll price[], ll n)
+int weight[1000],price[10000];
+int dp[100][100];
+int knap(int n,int W)
 {
-    ll i,w;
-    for(i=0;i<=n;i++)
-	{
-		for(w=0;w<=W;w++)
-		{
-			if(i==0 || w==0)
-				K[i][w]=0;
-			else if(weight[i-1] <= w)
-				K[i][w] = max( K[i-1][w] , K[i-1][w-weight[i-1]]+price[i-1] ) ;
-		   else K[i][w]=K[i-1][w];
-		}
-	}
-	return K[n][W];
+    if(n==0 || W==0) return 0;
+    if(dp[n][W]!=-1) return dp[n][W];
+    if(weight[n]>W) return dp[n][W]= knap(n-1,W);
+    return dp[n][W]= max((price[n]+knap(n-1,W-weight[n])), knap(n-1,W));
 }
-
 int main()
 {
-    ll t,i,j,k,l,m,n,w,x,y,z,W,ans;
-    cin>>n>>W;
-    for(i=0 ; i<n; i++)
-        cin>>weight[i]>>price[i];
-    ans = knapsack(W, weight, price, n);
-    cout<<ans<<endl;
+   int n,i,j,k,W;
+   cin>>n>>W;
+   for(i=1;i<=n;i++)
+    cin>>weight[i]>>price[i];
+    memset(dp,-1,sizeof dp);
+   k=knap(n,W);
+   cout<<k<<endl;
     return 0;
 }
